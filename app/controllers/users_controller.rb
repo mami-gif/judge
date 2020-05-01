b9c1ac86ccc6370de54eca8a31f0b1d1fa3be2d7
mami-gif <58882284+mami-gif@users.noreply.github.com>
2020-05-01 10:26:01 +0900

    user機能
    新規登録ページを作成しましたが、createができません。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,37 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+	
+	def show
+    	@user = User.find_by(id: params[:id])
+    end
+	
+	def new
+		@user = User.new
+	end	
+
+	def create
+		user = User.new(user_params)
+		if user.save
+		   redirect_to users_path, notice: "保存が成功しました"
+		else  
+		　 redirect_to new_user_path, notice: "保存が失敗しました"	
+		end
+	
 	def login_form
+
+	end	
+
+	def login
+		
+	
+
+	end	
+
+
+	private
+		def user_params
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :age, :female_male, :postal_code, :address, :tel)
+		end
 	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users
+get "signup" => "users#new"
+
 get"login" => "users#login_form"
+post"login" => "users#login"
 end
