c4019060a4b65bd3f8c5925ec0495a5d8f79b64d
mami-gif <58882284+mami-gif@users.noreply.github.com>
2020-05-05 00:50:36 +0900

    ログイン画面
    ログイン画面を作成しましたが、sessionが上手く起動しません。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	
+
 	def index
 		@users = User.all
 	end
@@ -16,23 +18,59 @@ class UsersController < ApplicationController
 		if user.save
 		   redirect_to users_path, notice: "保存が成功しました"
 		else  
-		　 redirect_to new_user_path, notice: "保存が失敗しました"	
+		　 render  new_user_path, notice: "保存が失敗しました"	
 		end
+	end
+
+	def edit
+		  @user = User.find(params[:id])
+	end
+
+	def update
+	  user = User.find(params[:id])
+	  if user.update(user_params)
+		 redirect_to users_path, notice: "更新しました"
+	  else
+		 redirect_to edit_user_path(user), notice: "更新が失敗しました"
+	  end
+		   
+	end
+		
+	def destroy
+	 	User.find(params[:id]).destroy		
+	 	redirect_to users_path
+	end
 	
 	def login_form
 
 	end	
 
+
 	def login
-		
+    	@user = User.find_by(email: params[:email], password: params[:password])
+    	if @user
+    	　　session[:user_id] = @user.id
+     	   flash[:notice] = "ログインしました"
+           redirect_to users_path
+   		else
+      	　　@error_message = "メールアドレスまたはパスワードが間違っています"
+     	   @email = params[:email]
+           @password = params[:password]
+           render login_form_user_path
+        end
 	
 
 	end	
+
+	def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+    end
 
 
 	private
 		def user_params
 			params.require(:user).permit(:first_name, :last_name, :email, :password, :age, :female_male, :postal_code, :address, :tel)
 		end
-	end	
-end
+end	
