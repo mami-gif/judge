c5d6a1b75388382ab052d6217b876a935ba6429c
mami-gif <58882284+mami-gif@users.noreply.github.com>
2020-05-06 12:22:27 +0900

    空行を無くして,form_tagからform_forに修正しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -50,7 +49,7 @@ class UsersController < ApplicationController
 	  session[:user_id] = @user.id
  	  flash[:notice] = "ログインしました"
       redirect_to users_path
-      
+
 	else
   	  @error_message = "メールアドレスまたはパスワードが間違っています"
  	  @email = params[:email]
